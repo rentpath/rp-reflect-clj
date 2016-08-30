86bc71e017e957a362ab96bec57774823e23b9be
Daniel Gregoire <daniel.l.gregoire@gmail.com>
2016-08-29 22:48:34 -0400

    Correct :url and :scm entries in project.clj

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
 (defproject com.rentpath/rp-reflect-clj "0.1.1-SNAPSHOT"
   :description "Reflection helpers for Clojure's clojure.lang.Reflector class"
-  :url "https://github.com/rentpath/rp-reflect"
+  :url "https://github.com/rentpath/rp-reflect-clj"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :scm {:url "git@github.com:rentpath/rp-reflect.git"}
+  :scm {:url "git@github.com:rentpath/rp-reflect-clj.git"}
   :deploy-repositories [["releases" :clojars]]
   :global-vars {*warn-on-reflection* true}
   :profiles {:dev {:dependencies [[org.clojure/clojure "1.8.0"]]}})
