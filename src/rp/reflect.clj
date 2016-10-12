4233623ff2e6b392e4787d455e5f434249e8fca6
Scott McLeod <halcyonblue@gmail.com>
2016-10-12 00:45:29 -0400

    [SM][000000] Add invoke-instance-method

--- src/rp/reflect.clj
+++ src/rp/reflect.clj
@@ -1,9 +1,14 @@
 (ns rp.reflect
-  (:import clojure.lang.Reflector))
+  (:import [clojure.lang Reflector]
+           [java.lang.reflect Method]))
 
 (defn- ^"[Ljava.lang.Object;" args-array
   [args]
   (into-array Object args))
+
+(defn- ^"[Ljava.lang.Class;" classes-array
+  [classes]
+  (into-array Class classes))
 
 (defn invoke-constructor
   "Given a Java `class` and a collection of `args`, invoke the constructor for the class with those arguments."
@@ -19,3 +24,13 @@
   "Given a Java `class`, a String field named `field-name`, return the value of the static field on the given class."
   [^Class class ^String field-name]
   (Reflector/getStaticField class field-name))
+
+(defn invoke-instance-method
+  "Given a Java `instance`, a String method named `method-name`, a collection of `parameter-types`, and a collection of `args`, invoke the method of the given instance with those arguments."
+  [^Object instance ^String method-name parameter-types & args]
+  (let [^Method method (.. instance
+                           getClass
+                           (getDeclaredMethod method-name
+                                              (classes-array parameter-types)))]
+    (. method (setAccessible true))
+    (. method (invoke instance (args-array args)))))
--- test/rp/reflect_test.clj
+++ test/rp/reflect_test.clj
@@ -23,3 +23,12 @@
          (get-static-field Long "MAX_VALUE")))
   (is (= 4
          (.getValue ^Month (get-static-field Month "APRIL")))))
+
+(deftest test-invoke-instance-method
+  (is (= 2
+         (invoke-instance-method "piebald"
+                                 "indexOf"
+                                 [Integer/TYPE]
+                                 (int \e))))
+  (is (= (str #"true|false")
+         (str (invoke-instance-method (java.util.Scanner. "") "boolPattern" [])))))
